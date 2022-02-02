69a8b3ada2256e43e77907dc51ba843d25d7a5a2
PatrickWyser <patrickwysi@hotmail.com>
2022-02-02 21:47:24 +0100

    some updates (with Problems) on feed

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -11,22 +11,18 @@
           </router-link>
         </i>
       </div>
-
     </div>
 
-
     <div class="posts" v-for="jam in jams" :key="jam.id">
-
       <div class="post">
-
         <div class="postHeader">
           <div class="postUser">
             <div class="postUserPic">
               <img src="https://picsum.photos/101" alt="ProfilePicture" />
             </div>
             <div class="postUserName">
-              {{jam.creator}}
-              </div>
+              {{ jam.creator }}
+            </div>
           </div>
         </div>
 
@@ -45,8 +41,10 @@
           </div>
         </div>
         <div class="postIcons">
-          <i> <font-awesome-icon :icon="['far', 'heart']" /> </i>
-          <i> <font-awesome-icon :icon="['far', 'comment']" /> </i>
+          <i @click="likeJam(jam.id)">
+            <font-awesome-icon :icon="['far', 'heart']" />
+          </i>
+          <i><font-awesome-icon :icon="['far', 'comment']" /> </i>
           <div class="directJam">
             <router-link to="/Jam"
               ><img src="../assets/icon.jpg" alt="jam"
@@ -54,7 +52,7 @@
           </div>
         </div>
         <div class="likeCounter">
-          <span>100 Likes</span>
+          <span>{{ jam.likes }} Likes</span>
         </div>
         <div class="postText">
           <p>
@@ -68,21 +66,21 @@
             <img src="https://picsum.photos/99" alt="ProfilePicture" />
           </div>
           <v-textarea
+            v-model="jam.vcomment"
             label="Add comment..."
             rows="1"
             auto-grow
             append-icon="mdi-send-circle-outline"
+            @click:append="comment(jam.id)"
           ></v-textarea>
         </div>
       </div>
-
     </div>
   </div>
-
 </template>
 
 <script>
- import axios from "axios";
+import axios from "axios";
 
 export default {
   data() {
@@ -90,24 +88,40 @@ export default {
       jams: [],
     };
   },
+
   async mounted() {
     this.jams = [];
-    var response= await axios
-    .get("jam/all")
+    var response = await axios.get("jam/all");
     for (let X = 0; X < response.data.length; X++) {
-    //  console.log(this.jams[X]);
-    
-    var resp =  await axios      
-      .get("jam?jamID=" + response.data[X])
-    this.jams.push(resp.data)
-     // console.log(this.jams)
-      axios
-      .get("user/" + this.jams[X].creator)
-      .then((creatorResp)=> { 
-        this.jams[X].creator = creatorResp.data.username;})
+      //  console.log(this.jams[X]);
 
+      var resp = await axios.get("jam?jamID=" + response.data[X]);
+      this.jams.push(resp.data);
+      // console.log(this.jams)
+      axios.get("user/" + this.jams[X].creator).then((creatorResp) => {
+        this.jams[X].creator = creatorResp.data.username;
+        //     this.jams[X].userPic = creatorResp.data.profilepicturepath;
+      });
+      axios.get("jam/likes?jamID=" + this.jams[X].id).then((likeResp) => {
+        this.jams[X].likes = likeResp.data.likeCount;
+      });
     }
-    
+  },
+
+  methods: {
+    likeJam(jamID) {
+      axios.post("jam/like", { jamID });
+    },
+    unLikeJam(jamID) {
+      axios.post("jam/unlike", { jamID });
+    },
+    comment(jamID) {
+      console.log(this.jams[jamID].vcomment);
+      axios.post("comment/create", {
+        jamID,
+        comment: this.jams[jamID].vcomment,
+      });
+    },
   },
 };
 </script>
